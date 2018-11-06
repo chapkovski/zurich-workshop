05d3039eeaa813473fea31807dcca27dcb0990a2
chapkovski <chapkovski@gmail.com>
2018-11-06 05:41:51 +0100

    otree-tools

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ from os import environ
 # in SESSION_CONFIGS, except those that explicitly override it.
 # the session config can be accessed from methods in your apps as self.session.config,
 # e.g. self.session.config['participation_fee']
-
+EXTENSION_APPS=['otree_tools']
 SESSION_CONFIG_DEFAULTS = {
     'real_world_currency_per_point': 1.00,
     'participation_fee': 0.00,
